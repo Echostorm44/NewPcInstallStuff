1bd35d082b32b828482055992d4d9a905870abe4
Echostorm <107306362+Echostorm44@users.noreply.github.com>
2025-10-22 16:08:32 -0400

    Add console encoding and import Terminal-Icons module
    Set console encoding to UTF-8 for PowerShell.

--- TerminalPimping/Microsoft.PowerShell_profile.ps1
+++ TerminalPimping/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,3 @@
 oh-my-posh --init --shell pwsh --config C:\Users\adamm\AppData\Local\Programs\oh-my-posh\themes\AdamCustom.json | Invoke-Expression
 Import-Module -Name Terminal-Icons
+[console]::InputEncoding = [console]::OutputEncoding = [System.Text.UTF8Encoding]::new()
